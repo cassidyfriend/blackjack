7e728176b3f022131698105cc63ef7c95edab57a
cassidyfriend <113076481+cassidyfriend@users.noreply.github.com>
2025-02-25 14:31:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+class card:
+    card = ''
+    value = 0
+    def __init__(self, card, value):
+        self.card = card
+        self.value = value
 
+cards = [
+Ace = card("Ace", 11)
+King = card("King", 10)
+Queen = card("Queen", 10)
+Jack = card("Jack", 10)
+Ten = card("Ten", 10)
+Nine = card("Nine", 9)
+Eight = card("Eight", 8)
+Seven = card("Seven", 7)
+Six = card("Six", 6)
+Five = card("Five", 5)
+Four = card("Four", 4)
+Three = card("Three", 3)
+Two = card("Two", 2)
+]
