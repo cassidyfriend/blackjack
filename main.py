862d022330abead8a7e45c9b4e202e341892ccb7
cassidyfriend <113076481+cassidyfriend@users.noreply.github.com>
2025-02-25 19:45:10 -0800

    update

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ class card:
         self.value = value
 
 cards = [
-Ace = card("Ace", 11)
-King = card("King", 10)
-Queen = card("Queen", 10)
-Jack = card("Jack", 10)
-Ten = card("Ten", 10)
-Nine = card("Nine", 9)
-Eight = card("Eight", 8)
-Seven = card("Seven", 7)
-Six = card("Six", 6)
-Five = card("Five", 5)
-Four = card("Four", 4)
-Three = card("Three", 3)
-Two = card("Two", 2)
+card("Ace", 11),
+card("King", 10),
+card("Queen", 10),
+card("Jack", 10),
+card("Ten", 10),
+card("Nine", 9),
+card("Eight", 8),
+card("Seven", 7),
+card("Six", 6),
+card("Five", 5),
+card("Four", 4),
+card("Three", 3),
+card("Two", 2)
 ]
